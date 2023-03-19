e10713738d3a036f83086849c13257a141ebc2e2
Andrey Ilyukhin <startopandrey@gmail.com>
2023-03-19 17:58:29 +0100

    logo change

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Head from 'next/head'
 
 const Layout = (props) => {
   return (
     <Fragment>
+       <Head>
+      <link rel="shortcut icon" href="/logo_small.png" />
+    </Head>
       <Header />
       <div>{props.children}</div>
       <Footer/>
