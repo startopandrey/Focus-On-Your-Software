58cefa37e28db8cd140a0879ed6edef86d4afe9d
Andrey Ilyukhin <startopandrey@gmail.com>
2023-05-24 18:01:05 +0200

    ads google connect

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -9,9 +9,19 @@ const Layout = (props) => {
       <Head>
         <link rel="shortcut icon" href="/logo_small.png" />
         <title>FOYS.</title>
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B0X0RC2NZR"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-B0X0RC2NZR');
+        </script>
       </Head>
       <Header />
-      <div style={{overflowX: 'hidden'}}>{props.children}</div>
+      <div style={{ overflowX: "hidden" }}>{props.children}</div>
       <Footer />
     </Fragment>
   );
