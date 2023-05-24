536e1c819f36c56c643d961db000906fe52e9950
Andrey Ilyukhin <startopandrey@gmail.com>
2023-05-24 18:27:17 +0200

    f

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -11,13 +11,7 @@ const Layout = (props) => {
         <link rel="shortcut icon" href="/logo_small.png" />
         <title>FOYS.</title>
 
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-B0X0RC2NZR"
-        ></Script>
-        <Script id="google-analytics" strategy="afterInteractive">{`( window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-B0X0RC2NZR');`}</Script>
+    
       </Head>
       <Header />
       <div style={{ overflowX: "hidden" }}>{props.children}</div>
